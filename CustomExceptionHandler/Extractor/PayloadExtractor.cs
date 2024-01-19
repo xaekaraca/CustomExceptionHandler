f7669caf5e0399ce6e61c08da99ad2b1bc05330b
Ahmet Emre Karaca <ahmetemre_k@hotmail.com>
2024-01-19 15:45:28 +0300

    Refactor exception payload extraction to new class
    The logic for extracting payloads from exception data has been moved from the ExceptionDataMapper to a new class called PayloadExtractor. This change follows single-responsibility principle and enhances code organization and readability.

---
+++ CustomExceptionHandler/Extractor/PayloadExtractor.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Ntt.Exceptions.Extractor;
+
+public static class PayloadExtractor
+{
+    public static async Task<JObject?> GetRawPayloadAsync(HttpContext httpContext)
+    {
+        var contentType = httpContext.Request.ContentType;
+
+        if (contentType == null)
+            return null;
+
+        try
+        {
+            if (contentType.Contains("application/json"))
+            {
+                if (httpContext.Request.Body.CanSeek)
+                {
+                    httpContext.Request.Body.Seek(0, SeekOrigin.Begin);
+                }
+
+                using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
+                var jsonPayload = await reader.ReadToEndAsync();
+                return JObject.Parse(jsonPayload);
+            }
+
+            if (contentType.Contains("multipart/form-data") || contentType.Contains("application/x-www-form-urlencoded"))
+            {
+                var formCollection = await httpContext.Request.ReadFormAsync();
+                var jsonObject = new JObject();
+
+                foreach (var field in formCollection)
+                {
+                    jsonObject.Add(field.Key, JToken.FromObject(field.Value.ToString()));
+                }
+
+                foreach (var file in httpContext.Request.Form.Files)
+                {
+                    jsonObject.Add(file.Name, new JObject
+                    {
+                        { "FileName", file.FileName },
+                        { "ContentType", file.ContentType },
+                    });
+                }
+
+                return jsonObject;
+            }
+        }
+        catch (Exception)
+        {
+            return new JObject
+            {
+                { "Error", "Failed to parse payload." }
+            };
+        }
+        
+        return null;
+    }
+}
--- CustomExceptionHandler/Mapper/ExceptionDataMapper.cs
+++ CustomExceptionHandler/Mapper/ExceptionDataMapper.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json.Linq;
+using Ntt.Exceptions.Extractor;
 using Ntt.Exceptions.Model;
 
 namespace Ntt.Exceptions.Mapper;
@@ -15,62 +16,8 @@ public static class ExceptionDataMapper
         exception.Data.Add("StatusCode", exceptionModel.StatusCode);
         exception.Data.Add("TargetSite", exception.TargetSite?.DeclaringType?.ToString());
         exception.Data.Add("Method", exception.TargetSite?.Name);
-        exception.Data.Add("Payload", GetRawPayloadAsync(context).GetAwaiter().GetResult());
+        exception.Data.Add("Payload", PayloadExtractor.GetRawPayloadAsync(context).GetAwaiter().GetResult());
         exception.Data.Add("Query", context.Request.QueryString.ToString());
         return exception;
-    }
-
-    private static async Task<JObject?> GetRawPayloadAsync(HttpContext httpContext)
-    {
-        var contentType = httpContext.Request.ContentType;
-
-        if (contentType == null)
-            return null;
-
-        try
-        {
-            if (contentType.Contains("application/json"))
-            {
-                if (httpContext.Request.Body.CanSeek)
-                {
-                    httpContext.Request.Body.Seek(0, SeekOrigin.Begin);
-                }
-
-                using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
-                var jsonPayload = await reader.ReadToEndAsync();
-                return JObject.Parse(jsonPayload);
-            }
-
-            if (contentType.Contains("multipart/form-data") || contentType.Contains("application/x-www-form-urlencoded"))
-            {
-                var formCollection = await httpContext.Request.ReadFormAsync();
-                var jsonObject = new JObject();
-
-                foreach (var field in formCollection)
-                {
-                    jsonObject.Add(field.Key, JToken.FromObject(field.Value.ToString()));
-                }
-
-                foreach (var file in httpContext.Request.Form.Files)
-                {
-                    jsonObject.Add(file.Name, new JObject
-                    {
-                        { "FileName", file.FileName },
-                        { "ContentType", file.ContentType },
-                    });
-                }
-
-                return jsonObject;
-            }
-        }
-        catch (Exception)
-        {
-            return new JObject
-            {
-                { "Error", "Failed to parse payload." }
-            };
-        }
-        
-        return null;
     }
 }
