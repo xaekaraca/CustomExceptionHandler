ff09fdabdae45dabc3ca8ab9547978275b597b79
xaekaraca <ahmetemre_k@hotmail.com>
2024-03-12 10:20:03 +0300

    Project name has been changed to NTT.ExceptionHandler

---
+++ NTT.ExceptionHandler/ExceptionTypes/OperationalException.cs
@@ -0,0 +1,13 @@
+namespace NTT.Exceptions.ExceptionTypes;
+
+[Serializable]
+public class OperationalException : Exception
+{
+    public OperationalException() { }
+
+    public OperationalException(string message) 
+        : base(message) { }
+
+    public OperationalException(string message, Exception innerException) 
+        : base(message, innerException) { }
+}
---
+++ NTT.ExceptionHandler/ExceptionTypes/UnauthorizedException.cs
@@ -0,0 +1,13 @@
+namespace NTT.Exceptions.ExceptionTypes;
+
+[Serializable]
+public class UnauthorizedException : Exception
+{
+    public UnauthorizedException() { }
+
+    public UnauthorizedException(string message) 
+        : base(message) { }
+
+    public UnauthorizedException(string message, Exception innerException) 
+        : base(message, innerException) { }
+}
